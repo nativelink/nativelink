7ea566f636f6f4ed37512b513af895d2fb70c4bc
allada <thegreatall@gmail.com>
2022-04-23 16:15:27 -0500

    Restructure LocalWorker for easier testing
    Abstracts away most of the LocalWorker implementation classes
    to enable easier unit testing.

--- cas/worker/running_actions_manager.rs
+++ cas/worker/running_actions_manager.rs
@@ -2,7 +2,7 @@
 
 use std::collections::HashMap;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::sync::{Arc, Weak};
 
 use fast_async_mutex::mutex::Mutex;
 
@@ -15,25 +15,68 @@ use proto::build::bazel::remote::execution::v2::Action;
 use proto::com::github::allada::turbo_cache::remote_execution::{ExecuteFinishedResult, StartExecute};
 use store::Store;
 
-type JobId = [u8; 32];
-struct RunningAction {}
+#[async_trait]
+pub trait RunningAction: Sync + Send + Sized + Unpin + 'static {
+    async fn prepare_action(self: Arc<Self>) -> Result<Arc<Self>, Error>;
+    async fn execute(self: Arc<Self>) -> Result<Arc<Self>, Error>;
+    async fn upload_results(self: Arc<Self>) -> Result<Arc<Self>, Error>;
+    async fn cleanup(self: Arc<Self>) -> Result<Arc<Self>, Error>;
+    async fn get_finished_result(self: Arc<Self>) -> Result<ExecuteFinishedResult, Error>;
+}
 
-impl RunningAction {
-    fn new(_action_info: ActionInfo, _salt: u64) -> Self {
+pub struct RunningActionImpl {}
+
+impl RunningActionImpl {
+    fn new() -> Self {
         Self {}
     }
 }
 
 #[async_trait]
-pub trait RunningActionsManager: Sync + Send + Sized + Unpin + 'static {
-    async fn start_action(self: Arc<Self>, start_execute: StartExecute) -> Result<ExecuteFinishedResult, Error>;
+impl RunningAction for RunningActionImpl {
+    /// Prepares any actions needed to execution this action. This action will do the following:
+    /// * Download any files needed to execute the action
+    /// * Build a folder with all files needed to execute the action.
+    /// This function will aggressively download and spawn potentially thousands of futures. It is
+    /// up to the stores to rate limit if needed.
+    async fn prepare_action(self: Arc<Self>) -> Result<Arc<Self>, Error> {
+        unimplemented!();
+    }
+
+    async fn execute(self: Arc<Self>) -> Result<Arc<Self>, Error> {
+        unimplemented!();
+    }
+
+    async fn upload_results(self: Arc<Self>) -> Result<Arc<Self>, Error> {
+        unimplemented!();
+    }
+
+    async fn cleanup(self: Arc<Self>) -> Result<Arc<Self>, Error> {
+        unimplemented!();
+    }
+
+    async fn get_finished_result(self: Arc<Self>) -> Result<ExecuteFinishedResult, Error> {
+        unimplemented!();
+    }
 }
+
+#[async_trait]
+pub trait RunningActionsManager: Sync + Send + Sized + Unpin + 'static {
+    type RunningAction: RunningAction;
+
+    async fn create_and_add_action(
+        self: Arc<Self>,
+        start_execute: StartExecute,
+    ) -> Result<Arc<Self::RunningAction>, Error>;
+}
+
+type ActionId = [u8; 32];
 
 /// Holds state info about what is being executed and the interface for interacting
 /// with actions while they are running.
 pub struct RunningActionsManagerImpl {
     cas_store: Pin<Arc<dyn Store>>,
-    running_actions: Mutex<HashMap<JobId, RunningAction>>,
+    running_actions: Mutex<HashMap<ActionId, Weak<RunningActionImpl>>>,
 }
 
 impl RunningActionsManagerImpl {
@@ -43,14 +86,11 @@ impl RunningActionsManagerImpl {
             running_actions: Mutex::new(HashMap::new()),
         }
     }
-}
 
-#[async_trait]
-impl RunningActionsManager for RunningActionsManagerImpl {
-    async fn start_action(self: Arc<Self>, start_execute: StartExecute) -> Result<ExecuteFinishedResult, Error> {
+    async fn create_action_info(&self, start_execute: StartExecute) -> Result<ActionInfo, Error> {
         let execute_request = start_execute
             .execute_request
-            .err_tip(|| "Expected execute_request to exist in LocalWorker")?;
+            .err_tip(|| "Expected execute_request to exist in StartExecute")?;
         let action_digest: DigestInfo = execute_request
             .action_digest
             .clone()
@@ -59,16 +99,28 @@ impl RunningActionsManager for RunningActionsManagerImpl {
         let action = get_and_decode_digest::<Action>(self.cas_store.as_ref(), &action_digest)
             .await
             .err_tip(|| "During start_action")?;
-        let action_info =
+        Ok(
             ActionInfo::try_from_action_and_execute_request_with_salt(execute_request, action, start_execute.salt)
-                .err_tip(|| "Could not create ActionInfo in start_action()")?;
+                .err_tip(|| "Could not create ActionInfo in create_and_add_action()")?,
+        )
+    }
+}
+
+#[async_trait]
+impl RunningActionsManager for RunningActionsManagerImpl {
+    type RunningAction = RunningActionImpl;
+
+    async fn create_and_add_action(
+        self: Arc<Self>,
+        start_execute: StartExecute,
+    ) -> Result<Arc<RunningActionImpl>, Error> {
+        let action_info = self.create_action_info(start_execute).await?;
+        let action_id = action_info.unique_qualifier.get_hash();
+        let running_action = Arc::new(RunningActionImpl::new());
         {
             let mut running_actions = self.running_actions.lock().await;
-            running_actions.insert(
-                action_info.unique_qualifier.get_hash(),
-                RunningAction::new(action_info, start_execute.salt),
-            );
+            running_actions.insert(action_id, Arc::downgrade(&running_action));
         }
-        Ok(ExecuteFinishedResult::default())
+        Ok(running_action)
     }
 }
